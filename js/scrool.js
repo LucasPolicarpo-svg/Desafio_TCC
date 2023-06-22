df06018a9508cefea1b97b05340a3a14ef8f16c3
DESKTOP-CK78SUU\Sofia Helena <empresariallrp@gmail.com>
2023-06-22 14:38:15 -0300

    feat add new module NUM(005)

---
+++ js/scrool.js
@@ -0,0 +1,17 @@
+const scrollTop = document.getElementById("scrolltop");
+
+window.onload = () =>{
+    scrollTop.style.visibility = "hidden";
+    scrollTop.style.opacity = 0;
+}
+
+window.onscroll = () =>{
+    if(window.scrollY > 200){
+        scrollTop.style.visibility = "visible";
+        scrollTop.style.opacity = 2;
+    }else{
+        scrollTop.style.visibility = "hidden";
+        scrollTop.style.opacity = 0;
+    }
+    
+}
